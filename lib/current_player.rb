801d950c4ea25b9fb3e44dbf489795a38ba8106a
BillyPittman1993 <billytrading93@gmail.com>
2018-10-02 02:41:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,6 @@ def turn_count?(board)
   if "move_is_met" 
     turn_count += 1 
 end
-brothers = ["Tim", "Tom", "Jim"]
-brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
 
 def current_player?(board)
 end
